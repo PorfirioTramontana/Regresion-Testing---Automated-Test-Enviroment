f18d94696aadf0596ec3875725c9481e2cd957fa
Pablo García Fernández <uo276824@uniovi.es>
2023-01-03 22:17:22 +0100

    Fix

--- src/main/java/regresion_testing/configuration/CsvExport.java
+++ src/main/java/regresion_testing/configuration/CsvExport.java
@@ -20,7 +20,8 @@ public class CsvExport {
 	}
 
 	public void export() {
-		try (CSVWriter writer = new CSVWriter(new FileWriter("output.csv"));) {
+		try (CSVWriter writer = new CSVWriter(
+				new FileWriter("src\\main\\resources\\output\\output.csv"));) {
 			writer.writeAll(csvExport);
 		} catch (IOException e) {
 			System.err.println("Error while exporting to .csv");
